fb0abe269e6b93eebd461da29a67e584c093c6d6
Frank Schoenmann <frank.schoenmann@de.bosch.com>
2018-06-13 11:44:42 +0200

    Byte swap for non-MSVC platforms.

--- inc/goldfish/common.h
+++ inc/goldfish/common.h
@@ -2,15 +2,23 @@
 
 #include <cstdint>
 #include <iterator>
+
+#ifdef _MSC_VER
 #include <stdlib.h>
+#define bswap_16(x) _byteswap_ushort(x)
+#define bswap_32(x) _byteswap_ulong(x)
+#define bswap_64(x) _byteswap_uint64(x)
+#else
+#include <byteswap.h>
+#endif
 
 namespace goldfish
 {
 	using byte = uint8_t;
 
-	inline uint16_t from_big_endian(uint16_t x) { return _byteswap_ushort(x); }
-	inline uint32_t from_big_endian(uint32_t x) { return _byteswap_ulong(x); }
-	inline uint64_t from_big_endian(uint64_t x) { return _byteswap_uint64(x); }
+	inline uint16_t from_big_endian(uint16_t x) { return bswap_16(x); }
+	inline uint32_t from_big_endian(uint32_t x) { return bswap_32(x); }
+	inline uint64_t from_big_endian(uint64_t x) { return bswap_64(x); }
 
 	inline uint16_t to_big_endian(uint16_t x) { return from_big_endian(x); }
 	inline uint32_t to_big_endian(uint32_t x) { return from_big_endian(x); }
